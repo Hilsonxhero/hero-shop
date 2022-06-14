0d2c749554f0c484830292cb960aab3c352f1290
Hilson <82933261+Hilsonxhero@users.noreply.github.com>
2022-06-14 12:49:39 +0430

    update main top slider

---
+++ src/components/app/main/slider/TopSlider.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <section class="mb-12">
+            <swiper class="" :loop="true" :modules="modules" :pagination="{ clickable: true }"
+                :navigation="{ nextEl: '.swiper-button-next-hero', prevEl: '.swiper-button-prev-hero', }">
+                <swiper-slide v-for="(item, index) in 4" :key="index">
+                    <div>
+                        <img src="media/banners/01.jpg" class="w-full  h-60 lg:h-96 object-cover" />
+                    </div>
+                </swiper-slide>
+
+                <div class="hx-main-swiper__actions flex items-center z-10 absolute right-0 bottom-2">
+                    <div class="swiper-button-prev-hero mr-8 ml-2" slot="button-prev">
+                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+                            <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
+                        </svg>
+
+                    </div>
+                    <div class="swiper-button-next-hero  " slot="button-next">
+                        <div class="flex">
+                            <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+                                <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>
+                            </svg>
+
+                        </div>
+                    </div>
+                </div>
+            </swiper>
+
+
+        </section>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SwiperClass, { Pagination, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+const modules = [Navigation, Pagination];
+</script>
+
+<style>
+.hx-main-swiper__actions {}
+
+.swiper-button-next-hero {}
+
+.swiper-button-prev-hero {}
+
+.swiper-button-next-hero,
+.swiper-button-prev-hero {
+    align-items: center;
+    color: #fff;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    margin-top: calc(0px - var(--swiper-navigation-size)/2);
+    z-index: 10;
+    border: 1px solid var(--color-neutral-300);
+    height: 40px;
+    width: 40px;
+    background-color: #333;
+    border-radius: 50%;
+}
+
+.swiper-button-next-hero,
+.swiper-rtl .swiper-button-prev-hero {
+    /* left: auto;
+    right: 10px; */
+}
+
+.swiper-button-next-hero,
+.swiper-rtl .swiper-button-next-hero {
+    /* right: auto;
+    left: 10px; */
+    /* right: 4rem; */
+}
+
+.swiper-pagination-bullet-active {
+    opacity: var(--swiper-pagination-bullet-opacity, 1);
+    background: #333;
+}
+
+@media (max-width : 1024px) {}
+</style>
--- src/views/app/landing.vue
+++ src/views/app/landing.vue
@@ -1,37 +1,13 @@
 <template>
   <div>
-    <div class="container">
-      <section class=" mt-20 w-full h-96">
-        <div class="grid grid-cols-12 content-center gap-4">
-
-          <div class="col-span-4">
-            <hx-button class="mr-2" variant="light">
-              <hx-icon right icon="user"></hx-icon>
-              حساب کاربری
-            </hx-button>
-
-            <hx-button class="mr-2" variant="light">
-              <hx-icon right icon="shopping-bag"></hx-icon>
-              سبد خرید
-            </hx-button>
-          </div>
-
-        </div>
-
-        <div class="w-full block mt-6"></div>
-
-        <hx-button variant="light" icon>
-          <hx-icon icon="search"></hx-icon>
-        </hx-button>
-
-
-      </section>
-    </div>
-
+    <section>
+      <TopSlider />
+    </section>
   </div>
 </template>
 
 <script setup lang="ts">
+import TopSlider from '@/components/app/main/slider/TopSlider.vue';
 
 </script>
 
